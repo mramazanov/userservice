1b9d67ea7d294e6e3dba560684870f4daa58e952
Admin <mramazanov@outlook.com>
2025-04-01 17:20:08 +0300

    init

---
+++ src/test/java/ru/javajabka/user/UserApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.javajabka.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
